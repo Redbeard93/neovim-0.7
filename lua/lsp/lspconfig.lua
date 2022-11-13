ee5efda84b4aa506cd47663696f016e2d342024a
ENKS <67786490+EN-KS@users.noreply.github.com>
2022-11-13 18:02:25 +0800

    Update lspconfig.lua

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -67,7 +67,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local servers = { 'pyright', 'gopls' }
 for _, lsp in pairs(servers) do
