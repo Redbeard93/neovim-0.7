f7ab03cbcf6de2e36c058fb9830328af41a8604e
ENKS <67786490+EN-KS@users.noreply.github.com>
2022-11-15 04:42:53 +0800

    Update config.lua

--- lua/basic/config.lua
+++ lua/basic/config.lua
@@ -11,7 +11,7 @@ end
 
 vim.cmd("autocmd InsertLeave * call fcitx5ToggleInput()")
 
---statusbar
+--statusline
 vim.cmd([[
 function! GitBranch()
     return system("git rev-parse --abbrev-ref HEAD 2>/dev/null | tr -d '\n'")
