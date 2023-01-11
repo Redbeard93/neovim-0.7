509f92034370938b8bbc887d4a1f06b5155f0f5d
Redbeard93 <67786490+Redbeard93@users.noreply.github.com>
2023-01-11 15:58:42 +0800

    Update settings.lua

--- lua/basic/settings.lua
+++ lua/basic/settings.lua
@@ -6,6 +6,21 @@
 --vim.opt.shellquote = ""
 --vim.opt.shellxquote = ""
 
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
+
 --fat cursor
 vim.opt.guicursor = ""
 
